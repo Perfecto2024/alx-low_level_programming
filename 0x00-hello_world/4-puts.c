465b05f314827dab5ab3588829fd91a17e847665
Perfecto2024 <alsaeed.m.khaer@gmail.com>
2022-12-08 14:07:54 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	pust("\"Programming is like building a multilingual puzzle");
+      return (0);
+}
