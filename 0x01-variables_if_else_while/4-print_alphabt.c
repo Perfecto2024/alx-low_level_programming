6ce15a683215fdb2d7b8bdbc3d7d8bca2406d4c9
Perfecto2024 <alsaeed.m.khaer@gmail.com>
2022-12-09 18:34:02 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
@@ -10,8 +11,8 @@
 int main(void)
 {
 	int i;
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
+
+	for (i = 0; i < 10; i++)printf("%d", i);
 	printf("\n");
 
 	return (0);
